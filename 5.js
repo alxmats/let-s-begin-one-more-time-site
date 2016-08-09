c88bfbe6081942c52afe8a91416dc252ce92fb64
Alexandr Matskevich <alxmats@gmail.com>
2016-08-09 09:24:01 +0300

    first test commit

---
+++ 5.js
@@ -0,0 +1,9 @@
+function pow(x, n) {
+  if (n != 1) { // пока n != 1, сводить вычисление pow(x,n) к pow(x,n-1)
+    return x * pow(x, n - 1);
+  } else {
+    return x;
+  }
+}
+
+alert( pow(2, 3) ); // 8
